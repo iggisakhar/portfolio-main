ce90735839662662e0af07f4569706507696d5be
iggisakhar <igorsakhar@gmail.com>
2024-04-04 11:42:45 -0700

    04/04

--- JavaScript_LapTop.js
+++ JavaScript_LapTop.js
@@ -105,3 +105,140 @@
 //     return `Sum of even numbers is ${sumOfEven}, sum of odd numbers is ${sumOfOdd}`
 // }
 
+// function sumOnlyNumbers(str){
+//     const numbers = { 0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true };
+//     if(str.length === 0) return 'String is empty';
+//     let sum = 0;
+//     let acc = '';
+//     for (let i = 0; i < str.length; i++) {
+//         const num = str[i];
+//         if (numbers[num]) acc += str[i];
+//         else {
+//             sum += Number(acc);
+//             acc = '';
+//         }
+//     }
+//     sum += Number(acc);
+//     return sum;
+// }
+
+// function sortNumbers(str){
+//     let firstStr = '';
+//     let secondStr = '';
+//     let digit = str.match(/\d/g) || [];
+//
+//     digit.forEach(el => {
+//         if(el >= '0' && el <= '4'){
+//             firstStr += el;
+//         } else if(el >= '5' && el <= '9'){
+//             secondStr += el;
+//         }
+//     })
+//     return [firstStr, secondStr];
+// }
+
+// function sumAllNumbers(str){
+//     const numbers = { 0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true };
+//     let sum = 0;
+//     let acc = '';
+//     for (let i = 0; i < str.length; i++) {
+//         const el = str[i];
+//         const nextEl = str[i + 1];
+//         if (numbers[el] || el === '-' && numbers[nextEl] && acc === '') {
+//             acc += el;
+//         }
+//
+//         else if(el === '-' && numbers[nextEl] && acc !== ''){
+//             sum += Number(acc);
+//             acc = '-';
+//         }
+//         else {
+//             sum += Number(acc);
+//             acc = '';
+//         }
+//     }
+//     sum += Number(acc);
+//     return sum;
+// }
+
+// function sumPositiveAndNegative(str){
+//     let str1 = str.split(', ');
+//     let sum = 0;
+//     for(let i = 0; i < str1.length; i++){
+//         if(!isNaN(str1[i]) && str1[i].trim() !== ''){
+//             sum += +str1[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// function sumPositiveAndNegative(str){
+//     let str1 = str.split(', ');
+//     let sum = 0;
+//     for(let i = 0; i < str1.length; i++){
+//         if(!isNaN(str1[i])){
+//             sum += Number(str1[i]);
+//         }
+//     }
+//     return sum;
+// }
+
+// function average(str){
+//     if(!str){
+//         return 'No data';
+//     }
+//     let str1 = str.split(' ');
+//     let sum = 0;
+//     let count = 0;
+//     for(let i = 0; i < str1.length; i++){
+//         let number = parseFloat(str1[i]);
+//         if(!isNaN(number)){
+//             sum += number;
+//             count++
+//         }
+//     }
+//     return count > 0 ? sum / count : 'No data';
+// }
+
+// function findAndSum(str){
+//     let numbers = '0123456789';
+//     let sum = 0;
+//     let chunk = '';
+//     for(let i = 0; i < str.length; i++) {
+//         if (numbers.includes(str[i])){
+//             chunk += str[i];
+//         } else {
+//             sum += Number(chunk);
+//             chunk = '';
+//         }
+//     }
+//     if (chunk !== '') {
+//         sum += Number(chunk);
+//     }
+//     return sum;
+// }
+
+// function sumOfFractionalNumbers(str){
+//     let sum = 0;
+//     let currNumber = '';
+//     let flag = false;
+//     let addToSum = () => {
+//         if(currNumber){
+//             sum += parseFloat(currNumber);
+//             currNumber = '';
+//             flag = false;
+//         }
+//     }
+//     for(let i of str + ' '){
+//         if(i >= '0' && i <= '9'){
+//             currNumber += i;
+//         } else if(i == '.' && !flag){
+//             currNumber += i;
+//             flag = true;
+//         }else{
+//             addToSum();
+//         }
+//     }
+//     return sum;
+// }
+
