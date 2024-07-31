29fdd679f13843896c6cfe35ba5bfb2a9c78e9c0
iggisakhar <igorsakhar@gmail.com>
2024-07-30 17:33:06 -0700

    07/30

--- JavaScript_LapTop.js
+++ JavaScript_LapTop.js
@@ -4462,3 +4462,23 @@
 //
 // console.log(checkElementsInArray(['a', 'b', 'c'], ['a', 'b', 'c', 'd', 'e']));
 
+// function checkElementsInArray(arr1, arr2){
+//
+//     for (let i = 0; i < arr1.length; i++) {
+//         let found = false;
+//
+//         for (let j = 0; j < arr2.length; j++) {
+//
+//             if (arr1[i] === arr2[j]) {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//
+//         if (!found) return false;
+//     }
+//
+//     return true;
+// }
+//
+// console.log(checkElementsInArray(['apple', 'banana', 'cherry'], ['banana', 'cherry', 'fig', 'date', 'apple']));
