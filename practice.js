45e510312c60c14fd78372d1084324f74a423839
igorsakhar <igorsakhar@gmail.com>
2025-01-03 15:56:16 -0800

    01/03

--- practice.js
+++ practice.js
@@ -12909,3 +12909,39 @@
 // // Initial render
 // renderNotes();
 
+// function bubbleSort(arr) {
+//     const n = arr.length;
+//     for (let i = 0; i < n - 1; i++) {
+//         for (let j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//             }
+//         }
+//     }
+//     return arr;
+// }
+//
+// const array = [64, 34, 25, 12, 22, 11, 90];
+// console.log("Original Array:", array);
+// console.log("Sorted Array:", bubbleSort(array));
+
+// function fibonacci(n) {
+//     if (n <= 0) return [];
+//     if (n === 1) return [0];
+//     const fib = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//         fib.push(fib[i - 1] + fib[i - 2]);
+//     }
+//     return fib;
+// }
+//
+// console.log("First 10 Fibonacci Numbers:", fibonacci(10));
+
+// function isPalindrome(str) {
+//     const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+//     const reversedStr = cleanedStr.split("").reverse().join("");
+//     return cleanedStr === reversedStr;
+// }
+//
+// console.log("Is 'racecar' a palindrome?", isPalindrome("racecar"));
+// console.log("Is 'hello' a palindrome?", isPalindrome("hello"));
