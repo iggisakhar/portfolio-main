ab21c84aeb5598cad3f7c51b03a7df55bab6cff0
igorsakhar <igorsakhar@gmail.com>
2025-01-22 19:59:17 -0800

    01/22

--- practice.js
+++ practice.js
@@ -13377,3 +13377,15 @@
 //
 //     return mat;
 // };
+
+// function countFrequencies(arr) {
+//     return arr.reduce((acc, item) => {
+//         acc[item] = (acc[item] || 0) + 1;
+//         return acc;
+//     }, {});
+// }
+//
+// const dataArray = ["apple", "banana", "apple", "orange", "banana", "apple", "grape"];
+// const frequencyResult = countFrequencies(dataArray);
+//
+// console.log("Element Frequencies:", frequencyResult);
