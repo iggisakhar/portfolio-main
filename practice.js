795dd938110baa8e40cb5c5102c115b0e5776a6f
igorsakhar <igorsakhar@gmail.com>
2025-03-01 14:11:50 -0800

    03/01

--- practice.js
+++ practice.js
@@ -13928,3 +13928,15 @@
 // testStrings.forEach(str => {
 //     console.log(`"${str}" is a palindrome: ${isPalindrome(str)}`);
 // });
+
+// function fizzBuzz() {
+//     for (let i = 1; i <= 100; i++) {
+//         let output = "";
+//         if (i % 3 === 0) output += "Fizz";
+//         if (i % 5 === 0) output += "Buzz";
+//         console.log(output || i);
+//     }
+// }
+//
+// // Run the FizzBuzz game
+// fizzBuzz();
