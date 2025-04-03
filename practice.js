41877cb12dc6efc507aa8118b693fbf38f442f37
igorsakhar <igorsakhar@gmail.com>
2025-04-02 22:38:40 -0700

    04/02

--- practice.js
+++ practice.js
@@ -14589,3 +14589,15 @@
 //         rl.close();
 //     }
 // });
+
+// function generatePassword(length = 12) {
+//     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}";
+//     let password = "";
+//     for (let i = 0; i < length; i++) {
+//         password += chars.charAt(Math.floor(Math.random() * chars.length));
+//     }
+//     return password;
+// }
+//
+// // Example usage:
+// console.log("Generated Password:", generatePassword(16));
