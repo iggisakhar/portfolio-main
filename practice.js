9a4b1ae1c4bb7d2bad2e1f1b99436648cbbef651
igorsakhar <igorsakhar@gmail.com>
2025-07-22 22:37:29 -0700

    07/22

--- practice.js
+++ practice.js
@@ -16990,3 +16990,22 @@
 //         return 'strong';
 //     });
 // }
+
+// function compress(str) {
+//     if (!str) return '';
+//
+//     let result = '';
+//     let count = 1;
+//
+//     for (let i = 1; i <= str.length; i++) {
+//         if (str[i] === str[i - 1]) {
+//             count++;
+//         } else {
+//             result += str[i - 1];
+//             if (count > 1) result += count;
+//             count = 1;
+//         }
+//     }
+//
+//     return result;
+// }
