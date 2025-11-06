bc5be923844f2db56127a91ff8d0ab27a54b6be1
igorsakhar <igorsakhar@gmail.com>
2025-11-05 22:07:11 -0800

    11/05

--- practice.js
+++ practice.js
@@ -21935,3 +21935,22 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+//
+// double GetMinutesAsHours(double origMinutes) {
+//
+//     return origMinutes / 60.0;
+//
+// }
+//
+// int main() {
+//     double minutes;
+//
+//     cin >> minutes;
+//
+//     // Will be run with 210.0, 3600.0, and 0.0.
+//     cout << GetMinutesAsHours(minutes) << endl;
+//
+//     return 0;
+// }
