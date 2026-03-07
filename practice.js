0156b6f2b5d6309d88273bfaef7c9608d97c4f31
igorsakhar <igorsakhar@gmail.com>
2026-03-06 22:31:11 -0800

    03/06

--- practice.js
+++ practice.js
@@ -27217,3 +27217,23 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// #include <string>
+// using namespace std;
+//
+// int main() {
+//     string firstString;
+//     string secondString;
+//
+//     cin >> firstString;
+//     cin >> secondString;
+//
+//     if(firstString < secondString){
+//         cout << firstString << " " << secondString << endl;
+//     }
+//     else {
+//         cout << secondString << " " << firstString << endl;
+//     }
+//     return 0;
+// }
+
