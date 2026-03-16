5b038c726ac7d10dd2397bde35b1fc6a4ec57d94
igorsakhar <igorsakhar@gmail.com>
2026-03-15 19:47:40 -0700

    03/15

--- practice.js
+++ practice.js
@@ -27519,3 +27519,21 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     const int NUM_ELEMENTS = 5;
+//     int runTimes[NUM_ELEMENTS];
+//     int i;
+//
+//     for (i = 0; i < NUM_ELEMENTS; ++i) {
+//         cin >> runTimes[i];
+//     }
+//
+//     cout << runTimes[0] << endl;
+//     cout << runTimes[1] << endl;
+//     cout << runTimes[2] << endl;
+//
+//     return 0;
+// }
