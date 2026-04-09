e048f1c30a6d474659c26109dbf701772748157e
igorsakhar <igorsakhar@gmail.com>
2026-04-08 23:10:34 -0700

    04/08

--- practice.js
+++ practice.js
@@ -29154,3 +29154,65 @@
 //
 //     return 0;
 // }
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+//
+// int recursions = 0;
+// int comparisons = 0;
+//
+// // Read integers from input and store them in a vector.
+// vector<int> ReadIntegers() {
+//     int size;
+//     cin >> size;
+//     vector<int> integers(size);
+//
+//     for (int i = 0; i < size; ++i) {
+//         cin >> integers.at(i);
+//     }
+//
+//     sort(integers.begin(), integers.end());
+//     return integers;
+// }
+//
+// int BinarySearch(int target, vector<int> integers, int lower, int upper) {
+//     recursions++;
+//
+//     int mid = (lower + upper) / 2;
+//
+//     comparisons++;
+//     if (target == integers.at(mid)) {
+//         return mid;
+//     }
+//
+//     if (lower == upper) {
+//         return -1;
+//     }
+//
+//     comparisons++;
+//     if (integers.at(mid) < target) {
+//         return BinarySearch(target, integers, mid + 1, upper);
+//     }
+//     else {
+//         return BinarySearch(target, integers, lower, mid - 1);
+//     }
+// }
+//
+// int main() {
+//     int target;
+//     int index;
+//
+//     vector<int> integers = ReadIntegers();
+//
+//     cin >> target;
+//
+//     index = BinarySearch(target, integers, 0, integers.size() - 1);
+//
+//     printf("index: %d, recursions: %d, comparisons: %d\n",
+//         index, recursions, comparisons);
+//
+//     return 0;
+// }
+
