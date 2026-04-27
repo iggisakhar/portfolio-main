f27c377222fefc83fef94d43ba31c49e18f8e861
igorsakhar <igorsakhar@gmail.com>
2026-04-26 22:28:41 -0700

    04/26

--- practice.js
+++ practice.js
@@ -29403,3 +29403,24 @@
 //
 //     return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+//
+// string ReverseString(string str) {
+//     if (str.length() <= 1) {
+//         return str;
+//     }
+//
+//     return ReverseString(str.substr(1)) + str[0];
+// }
+//
+// int main() {
+//     string input, result;
+//
+//     getline(cin, input);
+//     result = ReverseString(input);
+//     cout << "Reverse of \"" << input << "\" is \"" << result << "\"." << endl;
+//
+//     return 0;
+// }
