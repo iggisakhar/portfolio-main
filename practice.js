e2aa4be883325ef770c0f09ff6064e72d1a15b11
igorsakhar <igorsakhar@gmail.com>
2026-05-25 11:55:42 -0700

    05/25

--- practice.js
+++ practice.js
@@ -29995,3 +29995,81 @@
 //     return 0;
 // }
 
+// #include "LinkedListLibrary.h"
+// #include "VectorLibrary.h"
+// #include "BookNode.h"
+// #include "Book.h"
+// #include <fstream>
+// #include <iostream>
+// using namespace std;
+//
+// void FillLibraries(LinkedListLibrary &linkedListLibrary, VectorLibrary &vectorLibrary) {
+//     ifstream inputFS; // File input stream
+//     int linkedListOperations = 0;
+//     int vectorOperations = 0;
+//
+//     BookNode* currNode;
+//     Book tempBook;
+//
+//     string bookTitle;
+//     string bookAuthor;
+//     long long bookISBN;
+//
+//     // Try to open file
+//     inputFS.open("books.txt");
+//
+//     while(getline(inputFS, bookTitle)) {
+//         inputFS >> bookISBN;
+//         inputFS.ignore(1, '\n');
+//         getline(inputFS, bookAuthor);
+//
+//         // Insert into linked list and accumulate operation counts
+//         currNode = new BookNode(bookTitle, bookAuthor, bookISBN);
+//         linkedListOperations += linkedListLibrary.InsertSorted(currNode);
+//
+//         // Insert into vector and accumulate operation counts
+//         tempBook = Book(bookTitle, bookAuthor, bookISBN);
+//         vectorOperations += vectorLibrary.InsertSorted(tempBook);
+//     }
+//
+//     inputFS.close(); // close() may throw ios_base::failure if fails
+// }
+//
+// int main () {
+//     int linkedListOperations = 0;
+//     int vectorOperations = 0;
+//
+//     // Create libraries
+//     LinkedListLibrary linkedListLibrary = LinkedListLibrary();
+//     VectorLibrary vectorLibrary;
+//
+//     // Fill libraries with 100 books
+//     FillLibraries(linkedListLibrary, vectorLibrary);
+//
+//     // Create new book to insert into libraries
+//     BookNode* currNode;
+//     Book tempBook;
+//
+//     string bookTitle;
+//     string bookAuthor;
+//     long long bookISBN;
+//
+//     getline(cin, bookTitle);
+//     cin >> bookISBN;
+//     cin.ignore();
+//     getline(cin, bookAuthor);
+//
+// // Insert into linked list
+// // No need to delete currNode, deleted by LinkedListLibrary destructor
+//     currNode = new BookNode(bookTitle, bookAuthor, bookISBN);
+//     linkedListOperations = linkedListLibrary.InsertSorted(currNode);
+//
+// // Insert into VectorList
+//     tempBook  = Book(bookTitle, bookAuthor, bookISBN);
+//     vectorOperations = vectorLibrary.InsertSorted(tempBook);
+//
+// // Print number of operations
+//     cout << "Number of linked list book copy operations: " << linkedListOperations << endl;
+//     cout << "Number of vector book copy operations: " << vectorOperations << endl;
+//
+// }
